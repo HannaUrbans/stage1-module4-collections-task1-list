5fdfaa8bc83195c5c25a01bf4cd4a6bfe9b8c1db
Hanna_Urban <huan92@yandex.ru>
2024-02-25 17:19:39 +0300

    done

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,23 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
-    }
-}
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int element : sourceList) {
+
+                if (element % 2 != 0) {
+                    linkedList.addFirst(element);
+                } else {
+                    linkedList.addLast(element);
+                }
+
+        }
+
+        return linkedList;
+
+    }}
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,26 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
-    }
-}
+        Collections.sort(sourceList, new ListComparator());
 
-class ListComparator implements Comparator<String> {
-    @Override
-    public int compare(String a, String b) {
+    }
+
+    class ListComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+
+            int firstNumber = 5 * Integer.parseInt(a) * Integer.parseInt(a) + 3;
+            int secondNumber = 5 * Integer.parseInt(b) * Integer.parseInt(b) + 3;
+            if (firstNumber == secondNumber) {
+                return a.compareTo(b);
+            }
+            return Integer.compare(firstNumber, secondNumber);
+        }
     }
 }
